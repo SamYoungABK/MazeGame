61ccca14813b5037b11c59db44d02c1156ee7483
SamYoungABK <109538468+SamYoungABK@users.noreply.github.com>
2022-07-26 12:27:11 -0700

    Level Select menu now lists levels and has a cursor

--- Project/LevelSelectState.cpp
+++ Project/LevelSelectState.cpp
@@ -2,18 +2,25 @@
 
 #include <iostream>
 #include <conio.h>
+#include <string>
+#include <vector>
+#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+#include <experimental/filesystem>
+#include <iterator>
 
 #include "StateMachineExampleGame.h"
 
+
 using namespace std;
+using std::experimental::filesystem::directory_iterator;
+using std::string;
+using std::vector;
 
 constexpr int kEscapeKey = 27;
+constexpr int kEnterKey = 13;
 
-constexpr char kPlay = '1';
-constexpr char kHighScore = '2';
-constexpr char kSettings = '3';
-constexpr char kLevelSelect = '4';
-constexpr char kQuit = '5';
+constexpr char kUpArrow = 'w';
+constexpr char kDownArrow = 's';
 
 LevelSelectState::LevelSelectState(StateMachineExampleGame* pOwner)
 	: m_pOwner(pOwner)
@@ -21,7 +28,7 @@ LevelSelectState::LevelSelectState(StateMachineExampleGame* pOwner)
 }
 
 void LevelSelectState::Enter() {
-	/*GetLevelList();*/
+	GetLevelList();
 }
 
 bool LevelSelectState::Update(bool processInput)
@@ -30,25 +37,21 @@ bool LevelSelectState::Update(bool processInput)
 	if (processInput)
 	{
 		int input = _getch();
-		if (input == kEscapeKey || (char)input == kQuit)
+		if (input == kEscapeKey)
 		{
 			shouldQuit = true;
 		}
-		else if ((char)input == kPlay)
+		else if ((char)input == kUpArrow)
 		{
-			m_pOwner->LoadScene(StateMachineExampleGame::SceneName::Gameplay);
+			MoveCursorUp();
 		}
-		else if ((char)input == kHighScore)
+		else if ((char)input == kDownArrow)
 		{
-			m_pOwner->LoadScene(StateMachineExampleGame::SceneName::HighScore);
+			MoveCursorDown();
 		}
-		else if ((char)input == kLevelSelect)
+		else if ((char)input == kEnterKey)
 		{
-			m_pOwner->LoadScene(StateMachineExampleGame::SceneName::LevelSelect);
-		}
-		else if ((char)input == kSettings)
-		{
-			m_pOwner->LoadScene(StateMachineExampleGame::SceneName::Settings);
+
 		}
 	}
 	return shouldQuit;
@@ -59,4 +62,51 @@ void LevelSelectState::Draw()
 	system("cls");
 	cout << endl << endl << endl;
 	cout << "          - - - LEVEL SELECT - - -" << endl << endl;
+	cout << "        Controls:" << endl;
+	cout << "        W/S   - Move Cursor Up/Down" << endl;
+	cout << "        ENTER - Select Level" << endl << endl;
+	for (const auto& levelName : *m_levelList)
+	{
+		cout << "           " << IsLevelSelected(levelName) << " " << levelName << endl;
+	}
+		
+}
+
+void LevelSelectState::GetLevelList()
+{
+	string p = "../CustomLevels/";
+	vector<string> result;
+
+	delete m_levelList;
+	m_levelList = new std::vector<std::string>;
+	
+
+	for (const auto& entry : directory_iterator(p))
+	{
+		string filename = entry.path().filename().string();
+		m_levelList->push_back(filename);
+	}
+	m_selectedLevel = m_levelList->begin();
+}
+
+char LevelSelectState::IsLevelSelected(string levelName)
+{
+	if (*m_selectedLevel == levelName)
+		return '*';
+	else
+		return ' ';
+}
+
+void LevelSelectState::MoveCursorUp()
+{
+	if (m_selectedLevel == m_levelList->begin()) return;
+
+	m_selectedLevel--;
+}
+
+void LevelSelectState::MoveCursorDown()
+{
+	if (m_selectedLevel+1 == m_levelList->end()) return;
+
+	m_selectedLevel++;
 }
--- Project/LevelSelectState.h
+++ Project/LevelSelectState.h
@@ -1,18 +1,32 @@
 #pragma once
 #include "GameState.h"
+#include <vector>
+#include <string>
+#include <iterator>
 
 class StateMachineExampleGame;
 
 class LevelSelectState : public GameState
 {
 	StateMachineExampleGame* m_pOwner;
-
+private:
+	std::vector<std::string> *m_levelList = new std::vector<std::string>();
+	std::vector<std::string>::iterator m_selectedLevel;
+	
 public:
+	
 	LevelSelectState(StateMachineExampleGame* pOwner);
 	~LevelSelectState() = default;
 
 	virtual void Enter() override;
 	virtual bool Update(bool processInput = true) override;
 	virtual void Draw() override;
+
+private:
+	void GetLevelList();
+	char IsLevelSelected(std::string levelName);
+	void MoveCursorUp();
+	void MoveCursorDown();
+
 };
 
